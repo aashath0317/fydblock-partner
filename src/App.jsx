044d00fa5e2aac29ee88d88fb5237c2775a7206b
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2025-12-05 19:51:35 +0530

    Refactor App component to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+const App = () => {
+  return (
+    <Routes>
+      {/* Home Route */}
+      <Route path="/" element={<LandingPage />} />
+
+      {/* Auth Routes - Handling both /login and /signin conventions */}
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/signin" element={<SignIn />} />
+
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
+
+      {/* Placeholder for the missing bot-builder route mentioned in SignUp */}
+      <Route path="/bot-builder" element={<div className="text-white text-center pt-20">Bot Builder (Coming Soon)</div>} />
+    </Routes>
+  );
+};
+
+export default App;
