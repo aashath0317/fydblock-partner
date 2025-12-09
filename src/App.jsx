10ea41fbecae904a977175023df686c74b70c331
fazilmuhammad304 <fazilmuhammad304@gmail.com>
2025-12-09 21:15:41 +0530

    Add new routes for Payouts, Marketing, Support, and Profile

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,29 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Dashboard from './Dashboard';
 import Clients from './Clients';
+import Payouts from './Payouts';
+import Marketing from './Marketing';
+import Support from './Support';
+import Profile from './Profile'; // <--- Import this
 
 const App = () => {
   return (
     <div className="min-h-screen bg-[#050B0D] text-white font-sans relative overflow-x-hidden">
       <Routes>
-        {/* Public Routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/signup" element={<SignUp />} />
 
-        {/* Protected / Dashboard Routes */}
+        {/* Protected Routes */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/clients" element={<Clients />} />
+        <Route path="/payouts" element={<Payouts />} />
+        <Route path="/marketing" element={<Marketing />} />
+        <Route path="/support" element={<Support />} />
 
-        {/* Placeholders for menu items that don't have their own page yet */}
-        {/* You can point these to Dashboard or create new components for them later */}
-        <Route path="/payouts" element={<Dashboard />} />
-        <Route path="/marketing" element={<Dashboard />} />
-        <Route path="/support" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} /> {/* <--- New Route */}
       </Routes>
     </div>
   );
